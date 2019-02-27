e6daf11696aea5185679fd6e104fea90ba69093f
Surupa Chatterjee <chatterjee.surupa63@gmail.com>
2019-02-26 22:05:25 -0600

    changes in console log for well num

--- routes/api/wells.js
+++ routes/api/wells.js
@@ -10,7 +10,7 @@ const Well = require('../../models/Wells');
 // @access Public
 router.post('/getWell', (req, res) => {
         console.log(req.body.queryResult.parameters);
-        Well.find({'State':req.body.queryResult.parameters.geo-state}).then(well => {
+        Well.findOne({'WellNum':req.body.queryResult.parameters.WellNum}).then(well => {
             console.log(well);
             res.json(well);
             }
