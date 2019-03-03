7a8496aaaaefcee5bb566c937c3797b42404b654
Surupa Chatterjee <chatterjee.surupa63@gmail.com>
2019-03-02 21:46:49 -0600

    email service added

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const wells =  require('./routes/api/wells');
 const app =  express();
 
 //body parser middleware
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 //DB config
 const db = require('./config/keys').mongoURI;
